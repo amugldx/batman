6753456758cd8e3aab14f0cda47e6488943d84f1
amrmugl <meet.sagi.here@gmail.com>
2023-11-02 21:12:00 +0500

    reverted changes

--- pages/signup/index.vue
+++ pages/signup/index.vue
@@ -1,26 +1,5 @@
 <script setup lang="ts"></script>
 
 <template>
-	<div class="flex flex-col items-center justify-center mt-20">
-		<UiInput />
-		<UiButton intent="primary">Click</UiButton>
-		<UiButton
-			intent="primary"
-			:disable="true"
-			>Click</UiButton
-		>
-		<UiButton intent="secondary">Click</UiButton>
-		<UiButton
-			intent="secondary"
-			:loading="true"
-			>Click</UiButton
-		>
-		<UiButton intent="ghost">Click</UiButton>
-		<UiButton
-			intent="ghost"
-			:loading="true"
-			:disable="true"
-			>Click</UiButton
-		>
-	</div>
+	<div class="flex flex-col items-center justify-center mt-20"> hello </div>
 </template>
